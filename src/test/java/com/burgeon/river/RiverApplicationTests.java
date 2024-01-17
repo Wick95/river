d52256a3c7cd482d52dee28ef46204e33a8a37b7
liubojin <liubojin@meituan.com>
2024-01-17 21:04:21 +0800

    1.Init commit.

---
+++ src/test/java/com/burgeon/river/RiverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.burgeon.river;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
